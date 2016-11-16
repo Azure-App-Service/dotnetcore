36fda366d9605cd2f38a8a6d09870241dcbf1788
Your Name <you@example.com>
2016-11-15 16:06:26 -0800

    initial changes

---
+++ 1.0.0-preview2/src/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+
+namespace hostingstart
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls("http://*:80")
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
