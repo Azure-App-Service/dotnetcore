8e5c18c1b8233c60b1702b9d1deca6d0abf553e2
naziml <naziml@gmail.com>
2017-03-01 18:23:10 -0800

    Start ssh in init

--- 1.0.0-preview2-1/init_container.sh
+++ 1.0.0-preview2-1/init_container.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+service ssh start
 touch /home/LogFiles/dotnet_$WEBSITE_ROLE_INSTANCE_ID_out.log
 echo "$(date) Container started" >> /home/LogFiles/node_$WEBSITE_ROLE_INSTANCE_ID_out.log
 
