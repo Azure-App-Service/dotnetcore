869ef6bb6e380dbc92ac0219aedd20c5c074d661
Nick Walker <nickwalk@microsoft.com>
2017-04-04 08:42:13 -0700

    Additional .NET Core container quality-of-life updates
    - Created /home within the image to facility easy startup
      (works fine with no site drive mount)
    - Set ASPNETCORE_URLS to a default http://*:80 within the
      Dockerfile

--- 1.0.4-runtime/init_container.sh
+++ 1.0.4-runtime/init_container.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 service ssh start
+mkdir -p /home/LogFiles
 touch /home/LogFiles/dotnet_$WEBSITE_ROLE_INSTANCE_ID_out.log
 echo "$(date) Container started" >> /home/LogFiles/node_$WEBSITE_ROLE_INSTANCE_ID_out.log
 
