1d7602f7f2aaf5cf6fc8fb5d5635cbacf1d0811c
Michimune Kohno <mikono@microsoft.com>
2017-05-15 10:27:03 -0700

    Auto detect entrypoint DLL for .Net Core application

--- 1.1.1-runtime/init_container.sh
+++ 1.1.1-runtime/init_container.sh
@@ -4,10 +4,16 @@ mkdir -p /home/LogFiles
 touch /home/LogFiles/dotnet_$WEBSITE_ROLE_INSTANCE_ID_out.log
 echo "$(date) Container started" >> /home/LogFiles/node_$WEBSITE_ROLE_INSTANCE_ID_out.log
 
-if [ $# -eq 0 ]
-  then
-    cd /defaulthome/hostingstart
-    exec dotnet hostingstart.dll
-  else
-    exec "$@"
+[ $# -ne 0 ] && exec "$@"
+
+CSPROJ=`ls -1 /home/site/repository/*.csproj 2>/dev/null | head -1`
+
+if [ -n "$CSPROJ" ]; then
+  DLL=./`basename "$CSPROJ" .csproj`.dll
+  echo Found: $DLL
+  [ -e "$DLL" ] && exec dotnet "$DLL"
 fi
+
+echo Could not find .csproj or .dll. Using default.
+cd /defaulthome/hostingstart
+exec dotnet hostingstart.dll
