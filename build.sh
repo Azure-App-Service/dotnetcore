e16c2eb0b080404b9c6f5788fa53eb3a2bc8a649
Nick Walker <nickwalk@microsoft.com>
2017-06-29 17:45:40 -0700

    Revising dir names, build script

--- build.sh
+++ build.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 set -x -e
 
-docker build -t "$1"/dotnetcore:1.0.5 -t "$1"/dotnetcore:1.0 1.0
-docker build -t "$1"/dotnetcore:1.1.2 -t "$1"/dotnetcore:1.1 -t "$1"/dotnetcore:1 -t "$1"/dotnetcore:latest 1.1
+buildnumber=${4-$(date -u +"%y%m%d%H%M")}
+
+docker build -t "$1"/dotnetcore:1.0.5_"$buildnumber" -t "$1"/dotnetcore:lts_"$buildnumber" 1.0.5
+docker build -t "$1"/dotnetcore:1.1.2_"$buildnumber" -t "$1"/dotnetcore:latest_"$buildnumber" 1.1.2
 
 docker login -u "$2" -p "$3"
 
-docker push "$1"/dotnetcore:1.0.5
-docker push "$1"/dotnetcore:1.0
-
-docker push "$1"/dotnetcore:1.1.2
-docker push "$1"/dotnetcore:1.1
-docker push "$1"/dotnetcore:1
-docker push "$1"/dotnetcore:latest
+docker push "$1"/dotnetcore:lts_"$buildnumber"
+docker push "$1"/dotnetcore:1.0.5_"$buildnumber"
+docker push "$1"/dotnetcore:latest_"$buildnumber"
+docker push "$1"/dotnetcore:1.1.2_"$buildnumber"
 
 docker logout
