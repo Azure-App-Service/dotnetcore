c0792b5be8afb3f505e79bcd35150a92fb25a1f9
Nick Walker <nickwalk@microsoft.com>
2017-07-06 13:56:28 -0700

    Core 2.0 preview (based on microsoft/aspnetcore)

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
-#!/usr/bin/env bash
+!/usr/bin/env bash
 set -x -e
 
 buildnumber=${4-$(date -u +"%y%m%d%H%M")}
 
 docker build -t "$1"/dotnetcore:1.0.5_"$buildnumber" -t "$1"/dotnetcore:lts_"$buildnumber" 1.0.5
 docker build -t "$1"/dotnetcore:1.1.2_"$buildnumber" -t "$1"/dotnetcore:latest_"$buildnumber" 1.1.2
+docker build -t "$1"/dotnetcore:1.1.2_"$buildnumber" -t "$1"/dotnetcore:latest_"$buildnumber" 1.1.2
+docker build -t "$1"/dotnetcore:2.0.0-preview2_"$buildnumber" 2.0.0-preview2
 
 docker login -u "$2" -p "$3"
 
@@ -12,5 +14,6 @@ docker push "$1"/dotnetcore:lts_"$buildnumber"
 docker push "$1"/dotnetcore:1.0.5_"$buildnumber"
 docker push "$1"/dotnetcore:latest_"$buildnumber"
 docker push "$1"/dotnetcore:1.1.2_"$buildnumber"
+docker push "$1"/dotnetcore:2.0.0-preview2_"$buildnumber"
 
 docker logout
