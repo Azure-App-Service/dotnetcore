15772e685fb40311c5e5b366e37991b1ede707a1
Michimune Kohno <michimune@outlook.com>
2017-08-23 11:40:20 -0700

    Add .NET Core 2.0

---
+++ 2.0.0/build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+cd src
+sudo rm -rf bin obj publish
+docker run --rm -v $(pwd):/src -w /src microsoft/dotnet:2.0.0-sdk /bin/bash -c "dotnet restore && dotnet publish -o publish"
+cd publish
+zip -r ../../bin.zip *
---
+++ 2.0.0/src/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+
+namespace hostingstart
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .UseWebRoot("/home/site/wwwroot")
+                .Build();
+
+            host.Run();
+        }
+    }
+}
