ad6828340d74096948dddff6fdba668358f436d4
Lukasz Stempniewicz <lukaszcorgi@gmail.com>
2018-04-09 14:57:18 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@ set -euo pipefail
 
 buildnumber=${4-$(date -u +"%y%m%d%H%M")}
 
-docker build -t "$1"/dotnetcore:1.0.5_"$buildnumber" -t "$1"/dotnetcore:lts_"$buildnumber" 1.0.5
-docker build -t "$1"/dotnetcore:1.1.2_"$buildnumber" 1.1.2
-docker build -t "$1"/dotnetcore:2.0.0_"$buildnumber" 2.0.0
-docker build -t "$1"/dotnetcore:2.0.5_"$buildnumber" -t "$1"/dotnetcore:latest_"$buildnumber" 2.0.5
+docker build --no-cache -t "$1"/dotnetcore:1.0.5_"$buildnumber" -t "$1"/dotnetcore:lts_"$buildnumber" 1.0.5
+docker build --no-cache -t "$1"/dotnetcore:1.1.2_"$buildnumber" 1.1.2
+docker build --no-cache -t "$1"/dotnetcore:2.0.0_"$buildnumber" 2.0.0
+docker build --no-cache -t "$1"/dotnetcore:2.0.5_"$buildnumber" -t "$1"/dotnetcore:latest_"$buildnumber" 2.0.5
 docker tag "$1"/dotnetcore:latest_"$buildnumber" "$1"/dotnetcore:latest
 
 docker login -u "$2" -p "$3"
