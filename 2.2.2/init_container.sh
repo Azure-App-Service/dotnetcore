7913529c676a7bb75e98a9e57ebf34a4c23299ef
Patrick Lee <31744877+patricklee2@users.noreply.github.com>
2019-03-04 16:59:50 -0800

    fix ssh

--- 2.2.2/init_container.sh
+++ 2.2.2/init_container.sh
@@ -16,7 +16,7 @@ EOL
 cat /etc/motd
 
 # starting sshd process
-sed -i "s/{SSH_PORT}/$SSH_PORT/g" /etc/ssh/sshd_config
+sed -i "s/SSH_PORT/$SSH_PORT/g" /etc/ssh/sshd_config
 /usr/sbin/sshd
 
 [ -z "$ASPNETCORE_URLS" ] && export ASPNETCORE_URLS=http://*:"$PORT"
