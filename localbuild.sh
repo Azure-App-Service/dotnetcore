79937e89cd6d905f543879f95d92c734d47b4873
Patrick Lee <31744877+patricklee2@users.noreply.github.com>
2019-05-07 14:17:13 -0700

    remove minor versions

--- localbuild.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-buildnumber=${4-$(date -u +"%y%m%d%H%M")}
-
-docker build --no-cache -t "$1"/dotnetcore:2.2.2_"$buildnumber" -t "$1"/dotnetcore:latest_"$buildnumber" 2.2.2
-docker tag "$1"/dotnetcore:latest_"$buildnumber" "$1"/dotnetcore:latest
-
