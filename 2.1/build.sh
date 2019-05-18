2671205c38eab604aa1295ac2a9dc27e2388e3a8
appsvcbuild <patle@microsoft.com>
2019-05-17 17:39:18 -0700

    appsvcbuild

--- 2.1/build.sh
+++ 2.1/build.sh
@@ -4,3 +4,4 @@ sudo rm -rf bin obj publish
 docker run --rm -v $(pwd):/src -w /src microsoft/dotnet:2.1-sdk /bin/bash -c "dotnet restore && dotnet publish -o publish"
 cd publish
 zip -r ../../bin.zip *
+rm -rf ../publish ../out ../obj ../bin
