abe6210a7f9802eab2e4eb3b5538d5f1a9a8975b
appsvcbuild <patle@microsoft.com>
2019-05-17 17:35:14 -0700

    appsvcbuild

--- 1.0/build.sh
+++ 1.0/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd src
 sudo rm -rf bin obj publish
-docker run --rm -v $(pwd):/src -w /src microsoft/dotnet:1.1-sdk /bin/bash -c "dotnet restore && dotnet publish -o publish"
+docker run --rm -v $(pwd):/src -w /src microsoft/dotnet:1.0-sdk /bin/bash -c "dotnet restore && dotnet publish -o publish"
 cd publish
 zip -r ../../bin.zip *
--- 2.2/init_container.sh
+++ 2.2/init_container.sh
@@ -19,51 +19,27 @@ cat /etc/motd
 sed -i "s/SSH_PORT/$SSH_PORT/g" /etc/ssh/sshd_config
 /usr/sbin/sshd
 
-[ -z "$ASPNETCORE_URLS" ] && export ASPNETCORE_URLS=http://*:"$PORT"
+appPath="/home/site/wwwroot"
+runFromPath="/tmp/webapp"
+startupCommandPath="/opt/startup/startup.sh"
+defaultAppPath="/defaulthome/hostingstart/hostingstart.dll"
+userStartupCommand="$@"
 
-# If there is any command line argument specified, do basic validation and execute.
-if [ $# -ne 0 ]; then
-    echo "App Command Line configured: $@"
-    if [ $# -eq 1 ]; then
-        # ensure $1 exists on disk
-        if [ -f $1 ]; then
-            echo "App Command Line file on disk, executing it."
-            exec "$@"
-        else
-            echo "App command Line file doesn't exist on disk, fallback to auto-detect"
-        fi
-    elif [ $# -eq 2 -a $1 == "dotnet" ]; then
-        if [ -f $2 ]; then
-            echo "Argument to dotnet exists on disk, launching it: $@"
-       	    exec "$@"
-        else
-            echo "Argument to dotnet doesn't exist on disk, fallback to auto-detect"
-        fi
-    else
-        exec "$@"
-    fi
+# When run from copy is enabled, Oryx tries to run the app from a different directory (local to the container),
+# so sanitize any input arguments which still reference the wwwroot path. This is true for VS Publish scenarios.
+# Even though VS Publish team might fix this on their end, end users might not have upgraded their extension, so
+# this code needs to be present.
+if [ "$APP_SVC_RUN_FROM_COPY" = true ]; then
+    # Trim the ending '/'
+    appPath=$(echo "${appPath%/}")
+    runFromPath=$(echo "${runFromPath%/}")
+    userStartupCommand=$(echo $userStartupCommand | sed "s!$appPath!$runFromPath!g")
+    runFromPathArg="-runFromPath $runFromPath"
 fi
 
-# Pick up one .csproj file from repository where git push puts files into
-CSPROJ=`ls -1 /home/site/repository/*.csproj 2>/dev/null | head -1`
+oryxArgs="-appPath $appPath -output $startupCommandPath -defaultAppFilePath $defaultAppPath \
+    -bindPort $PORT -userStartupCommand '$userStartupCommand' $runFromPathArg"
 
-# Convert /home/site/repository/<name>.csproj into ./<name>.dll and execute it
-if [ -n "$CSPROJ" ]; then
-    DLL=./`basename "$CSPROJ" .csproj`.dll
-    if [ -e $DLL ]; then
-        echo Found: $DLL
-        exec dotnet "$DLL"
-    else
-        if [ -d /home/site/wwwroot/oryx_publish_output ]; then
-            DLL=./oryx_publish_output/`basename "$CSPROJ" .csproj`.dll
-            if [ -e $DLL ]; then
-                echo Found: $DLL
-                exec dotnet "$DLL"
-            fi
-        fi
-    fi
-fi
-
-echo Could not find .csproj or .dll. Using default.
-cd /defaulthome/hostingstart
-exec dotnet hostingstart.dll
+echo "Running oryx $oryxArgs"
+eval oryx $oryxArgs
+$startupCommandPath
