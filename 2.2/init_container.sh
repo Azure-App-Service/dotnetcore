4a8a63f8f58a8bfb8715caca9ee955360b2f9357
blimp <patle@microsoft.com>
2019-06-26 16:13:30 -0700

    blimp

--- 2.2/init_container.sh
+++ 2.2/init_container.sh
@@ -15,6 +15,9 @@ Dotnet quickstart: https://aka.ms/dotnet-qs
 EOL
 cat /etc/motd
 
+# Get environment variables to show up in SSH session
+eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
+
 # starting sshd process
 sed -i "s/SSH_PORT/$SSH_PORT/g" /etc/ssh/sshd_config
 /usr/sbin/sshd
